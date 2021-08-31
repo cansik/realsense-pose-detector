cc7e28c6bc2753627fb9fa1bd790bc7fb7abacb7
Florian Bruggisser <florian@bildspur.ch>
2021-08-31 16:11:52 +0200

    implemented media pipe pose detector and updated to most recent version

--- README.md
+++ README.md
@@ -1,6 +1,76 @@
 # RealSense Pose Detector
 A simple pose detector which runs with the realsense framework.
 
-![](readme/pose_tracking_upper_body_landmarks.png)
+### Install & Run
+
+Currently this is only tested on Windows and macOS. It's recommended to use Python3 (`>3.7`) and a virtual environment.
+
+```bash
+python install -r requirements.txt
+```
+
+To run an example use the basic python command to start up the script.
+
+```bash
+# start pose detection with realsense camera
+python pose.py
+```
+
+### Full-Body Pose Landmark Model (BlazePose Tracker)
+The landmark model currently included in MediaPipe Pose predicts the location of 33 full-body landmarks (see figure below), each with (`x, y, z, visibility`). Note that the z value should be discarded as the model is currently not fully trained to predict depth, but this is something we have on the roadmap.
+
+![Pose Description](readme/pose_tracking_full_body_landmarks.png)
+
+*[Reference: mediapipe/solutions/pose](https://google.github.io/mediapipe/solutions/pose#pose-landmark-model-blazepose-tracker)*
+
+#### Format
+
+- `count` - Indicates how many poses are detected (currently only `0` or `1`)
+- list of landmarks (`33` per pose) (if pose has been detected)
+    - `x` - X-Position of the landmark
+    - `y` - Y-Position of the landmark
+    - `z` - Z-Position of the landmark
+    - `visibility` - Visibility of the landmark
+
+```
+/mediapipe/pose [count, x, y, z, visibility, x, y, z, visibility ...]
+```
+
+### Help
+
+```
+usage: pose.py [-h] [--resolution width height] [--fps FPS]
+               [--model-complexity MODEL_COMPLEXITY] [--no-smooth-landmarks]
+               [--static-image-mode] [-mdc MIN_DETECTION_CONFIDENCE]
+               [-mtc MIN_TRACKING_CONFIDENCE] [--ip IP] [--port PORT]
+
+optional arguments:
+  -h, --help            show this help message and exit
+
+RealSense:
+  --resolution width height
+                        Resolution of the realsense stream.
+  --fps FPS             Framerate of the realsense stream.
+
+MediaPipe:
+  --model-complexity MODEL_COMPLEXITY
+                        Set model complexity (0=Light, 1=Full, 2=Heavy).
+  --no-smooth-landmarks
+                        Disable landmark smoothing.
+  --static-image-mode   Enables static image mode.
+  -mdc MIN_DETECTION_CONFIDENCE, --min-detection-confidence MIN_DETECTION_CONFIDENCE
+                        Minimum confidence value ([0.0, 1.0]) for the
+                        detection to be considered successful.
+  -mtc MIN_TRACKING_CONFIDENCE, --min-tracking-confidence MIN_TRACKING_CONFIDENCE
+                        Minimum confidence value ([0.0, 1.0]) to be considered
+                        tracked successfully.
+
+Network:
+  --ip IP               The ip of the OSC server
+  --port PORT           The port the OSC server is listening on
+```
+
+### About
+MIT License - Copyright (c) 2021 Florian Bruggisser
 
 Based on the ideas of [mediapipe-osc](https://github.com/cansik/mediapipe-osc).
--- pose.py
+++ pose.py
@@ -9,8 +9,6 @@ from pythonosc import udp_client
 from pythonosc.osc_message_builder import OscMessageBuilder
 import pyrealsense2 as rs
 
-from utils import add_default_args
-
 OSC_ADDRESS = "/mediapipe/pose"
 
 
@@ -35,7 +33,28 @@ def send_pose(client: udp_client,
 def main():
     # read arguments
     parser = argparse.ArgumentParser()
-    add_default_args(parser)
+    rs_group = parser.add_argument_group("RealSense")
+    rs_group.add_argument("--resolution", default=[640, 480], type=int, nargs=2, metavar=('width', 'height'),
+                          help="Resolution of the realsense stream.")
+    rs_group.add_argument("--fps", default=30, type=int,
+                          help="Framerate of the realsense stream.")
+
+    mp_group = parser.add_argument_group("MediaPipe")
+    mp_group.add_argument("--model-complexity", default=1, type=int,
+                          help="Set model complexity (0=Light, 1=Full, 2=Heavy).")
+    mp_group.add_argument("--no-smooth-landmarks", action="store_false", help="Disable landmark smoothing.")
+    mp_group.add_argument("--static-image-mode", action="store_true", help="Enables static image mode.")
+    mp_group.add_argument("-mdc", "--min-detection-confidence", type=float, default=0.5,
+                          help="Minimum confidence value ([0.0, 1.0]) for the detection to be considered successful.")
+    mp_group.add_argument("-mtc", "--min-tracking-confidence", type=float, default=0.5,
+                          help=" Minimum confidence value ([0.0, 1.0]) to be considered tracked successfully.")
+
+    nw_group = parser.add_argument_group("Network")
+    nw_group.add_argument("--ip", default="127.0.0.1",
+                          help="The ip of the OSC server")
+    nw_group.add_argument("--port", type=int, default=7400,
+                          help="The port the OSC server is listening on")
+
     args = parser.parse_args()
 
     # create osc client
@@ -46,13 +65,19 @@ def main():
     mp_pose = mp.solutions.pose
 
     pose = mp_pose.Pose(
-        min_detection_confidence=args.min_detection_confidence, min_tracking_confidence=args.min_tracking_confidence)
+        smooth_landmarks=args.no_smooth_landmarks,
+        static_image_mode=args.static_image_mode,
+        model_complexity=args.model_complexity,
+        min_detection_confidence=args.min_detection_confidence,
+        min_tracking_confidence=args.min_tracking_confidence)
 
     # create realsense pipeline
     pipeline = rs.pipeline()
 
+    width, height = args.resolution
+
     config = rs.config()
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, 30)
 
     profile = pipeline.start(config)
 
@@ -90,7 +115,7 @@ def main():
             prev_frame_time = current_time
 
             cv2.putText(image, "FPS: %.0f" % fps, (7, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 1, cv2.LINE_AA)
-            cv2.imshow('MediaPipe OSC Pose', image)
+            cv2.imshow('RealSense Pose Detector', image)
 
             if cv2.waitKey(5) & 0xFF == 27:
                 break
